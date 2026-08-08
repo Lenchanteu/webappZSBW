c12dfcb9f33e49c38b11e9dccc096c7123e6f31a
Lenchanteu <vancranemmerlin@gmail.com>
2026-08-08 15:37:48 +0200

    Add i18n to templates and emails
    Wrap UI strings in gettext across many templates, update template copy (move from French to translatable English strings), and internationalize email_sender subjects and bodies using flask_babel.gettext (_). Add French translations file translations/fr/LC_MESSAGES/messages.po containing extracted message IDs. No functional logic changes.

--- email_sender.py
+++ email_sender.py
@@ -2,6 +2,7 @@ import smtplib
 import os
 from email.message import EmailMessage
 from dotenv import load_dotenv
+from flask_babel import gettext as _
 
 load_dotenv()
 EMAIL = os.getenv('SMTP_EMAIL')
@@ -15,7 +16,7 @@ def send_bug_report(report_folder):
     msg["From"] = EMAIL
     msg["To"] = EMAIL
 
-    msg.set_content("A new bug report has been submitted from the ZSBW app.")
+    msg.set_content("A new bug report has been submitted from the app.")
 
     # Attach every file in the report folder
     for filename in os.listdir(report_folder):
@@ -35,10 +36,13 @@ def send_bug_report(report_folder):
         
 def send_confirmation_email(user, email_addrs, url):
     msg = EmailMessage()
-    msg["Subject"] = "Confirmation de l'email, App ZSBW"
+    msg["Subject"] = _("Confirmation of your email")
     msg["From"] = EMAIL #temporary
     msg["To"] = email_addrs
-    msg.set_content(f"A l'utilisateur {user},\n Vous avez créé(e) un compte pour l'application ZSBW. Veuillez confirmer votre addresse email pour pouvoir utiliser l'application. Pour ce faire, veuillez cliquer sur ce lien: {url}.\n Merci")
+    msg.set_content(_("To user %(user)s,\n You have created an account for the ZSBW application. Please confirm your email address to use the application. To do so, please click on this link: %(url)s.\n Thank you") % {
+        "user": user,
+        "url": url
+    })
     
     with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:  # type: ignore
         smtp.login(EMAIL, PASSWORD) # type: ignore
@@ -46,10 +50,13 @@ def send_confirmation_email(user, email_addrs, url):
 
 def send_passcode(user, email_addrs, passcode):
     msg = EmailMessage()
-    msg["Subject"] = "Code pour le changement de votre mot de passe"
+    msg["Subject"] = _("Code to change your password")
     msg["From"] = EMAIL
     msg["To"] = email_addrs
-    msg.set_content(f"À l'utilisateur {user}, \n Vous avez fait une demande de changement de mot de passe. Afin de confirmer votre identité, voici le code pour autoriser le changement {passcode}. \n Vous n'avez pas fait de demande de changement de mot de passe? Quelqu'un à peut-être réussi à avoir votre mot de passe, nous vous reccomendons de changer votre mot de passe et de vérifier les dernières connections à votre compte.")
+    msg.set_content(_("To user %(user)s, You have requested a password change. To confirm your identity, here is the code to authorize the change: %(passcode)s. Didn't request a password change? Someone may have obtained your password; we recommend that you change your password and check the latest logins to your account.") % {
+        "user": user,
+        "passcode": passcode
+    })
     with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp: #type: ignore
         smtp.login(EMAIL, PASSWORD) # type: ignore
         smtp.send_message(msg)
